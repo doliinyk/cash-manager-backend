de4165c935d4a4783e049d45c75ae4e9ddb6a0c2
DedRexst <hlynpavlo@gmail.com>
2024-02-24 13:52:49 +0200

    set up spring security

--- src/main/java/com/cashmanagerbackend/configs/JWTFilter.java
+++ src/main/java/com/cashmanagerbackend/configs/JWTFilter.java
@@ -11,10 +11,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
-
+@Component
 public class JWTFilter extends OncePerRequestFilter {
     private final JWTUtil jwtUtil;
     private final UserDetailsServiceImplementation userDetailsService;
--- src/main/java/com/cashmanagerbackend/security/UserDetailsImplementation.java
+++ src/main/java/com/cashmanagerbackend/security/UserDetailsImplementation.java
@@ -7,6 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 
+
 public class UserDetailsImplementation implements UserDetails {
 
     private final User user;
