48c19ac7202bbd6275f13728c4a43b9094ba1641
doliinyk <denysoliinyk@outlook.com>
2024-04-02 23:45:22 +0300

    Renamed main class test name & add coverage plugin

--- src/test/java/com/cashmanagerbackend/CashManagerBackendApplicationTest.java
+++ src/test/java/com/cashmanagerbackend/CashManagerBackendApplicationTest.java
@@ -7,7 +7,7 @@ import org.springframework.context.ApplicationContext;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
-class CashManagerBackendApplicationTests {
+class CashManagerBackendApplicationTest {
     @Test
     void contextLoads(ApplicationContext applicationContext) {
         assertNotNull(applicationContext);
